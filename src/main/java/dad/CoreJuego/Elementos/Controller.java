d5832c48e1eb156ac8f282bfaab65deb29c617e9
GabrielPerezMinik <91058454+GabrielPerezMinik@users.noreply.github.com>
2023-02-23 10:49:06 +0000

    Update Controller.java

--- src/main/java/dad/CoreJuego/Elementos/Controller.java
+++ src/main/java/dad/CoreJuego/Elementos/Controller.java
@@ -2,7 +2,6 @@ package dad.CoreJuego.Elementos;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -10,8 +9,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class Controller implements Initializable {
 
@@ -45,15 +42,12 @@ public class Controller implements Initializable {
 		// game.fpsProperty().addListener((o, ov, nv) -> System.out.println(nv +
 		// "fps"));
 		game.start();
-
-		Media media= new Media(Paths.get("test.mp3").toUri().toString());
-		MediaPlayer mediaPlayer= new MediaPlayer(media);
-		mediaPlayer.play();
+		
 	}
 
 	public BorderPane getView() {
 		return view;
 	}
-
+	
 	
 }
