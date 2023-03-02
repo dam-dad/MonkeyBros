63425186887c9200a0b30c33802a224763e6d979
DavidAlejandroHA <99730787+DavidAlejandroHA@users.noreply.github.com>
2023-03-02 23:23:53 +0000

    Cambio a id numérica dependiente del número de registros de jugadores en el diploma

--- src/main/java/dad/Api/GlobalStat/GlobalStat.java
+++ src/main/java/dad/Api/GlobalStat/GlobalStat.java
@@ -26,7 +26,8 @@ import javafx.scene.control.Alert.AlertType;
 
 public class GlobalStat {
 
-	String clientId="nL8l9XT08r7gzzsaPC3f2Dhyu1pjqOWOA4aJ23Ja", clientSecret="Rq4qTegwhwyUN6pPvJfkF3UJeEMinWMenRFg5WMn";
+	String clientId = "nL8l9XT08r7gzzsaPC3f2Dhyu1pjqOWOA4aJ23Ja",
+			clientSecret = "Rq4qTegwhwyUN6pPvJfkF3UJeEMinWMenRFg5WMn";
 	final String token;
 	GlobalStats client;
 	Stats stats;
@@ -34,7 +35,8 @@ public class GlobalStat {
 	/**
 	 * Constructor que conecta el cliente y le da el Token de acceso
 	 * 
-	 * @throws Exception lanza una expcecion en caso de fallar algo impidieno continuar
+	 * @throws Exception lanza una expcecion en caso de fallar algo impidieno
+	 *                   continuar
 	 */
 	public GlobalStat() throws Exception {
 		client = new GlobalStats(clientId, clientSecret);
@@ -44,28 +46,31 @@ public class GlobalStat {
 
 	/**
 	 * Cuando se quiera registrar un nuevo usuario
+	 * 
 	 * @param nombre
 	 * @return
 	 * @throws Exception
 	 */
 	@SuppressWarnings("serial")
 	public Stats createUser(String nombre) throws Exception {
-		stats = client.createStats(nombre, new HashMap<String, Object>() {{
-			
-			put("Score", 0);
-			
-		}});
-		
+		stats = client.createStats(nombre, new HashMap<String, Object>() {
+			{
+
+				put("Score", 0);
+
+			}
+		});
+
 		Properties prop = new Properties();
 		try {
 			prop.load(new FileInputStream(RootMenuController.RUTA_PLAYER_IDS));
-			prop.setProperty(stats.getId(), stats.getName());
+			prop.setProperty(stats.getId(), prop.size() + 1 + "");
 			prop.store(new FileOutputStream(RootMenuController.RUTA_PLAYER_IDS), "");
 		} catch (FileNotFoundException e) {
-			if(!RootMenuController.RUTA_PLAYERS_FOLDER.exists()) {
+			if (!RootMenuController.RUTA_PLAYERS_FOLDER.exists()) {
 				RootMenuController.RUTA_PLAYERS_FOLDER.mkdir();
 			}
-			prop.setProperty(stats.getId(), stats.getName());
+			prop.setProperty(stats.getId(), prop.size() + 1 + "");
 			prop.store(new FileOutputStream(RootMenuController.RUTA_PLAYER_IDS), "");
 		} catch (IOException e1) {
 			Alert alerta = new Alert(AlertType.ERROR);
@@ -80,41 +85,42 @@ public class GlobalStat {
 
 	@SuppressWarnings("serial")
 	public void updateUser(String s) throws Exception {
-		Stats stats = client.updateStats(s, 
-				new HashMap<String, Object>() {{
-					
+		Stats stats = client.updateStats(s, new HashMap<String, Object>() {
+			{
+
 				put("Score", "+20");
-				
-			}});
+
+			}
+		});
 		System.out.println(stats);
 	}
-	
+
 	@SuppressWarnings("serial")
 	public void resetUserScore(String s) throws Exception {
 		int points = client.getStatsSection(s, "Score").getUserRank().getValue();
-		Stats stats = client.updateStats(s, 
-				new HashMap<String, Object>() {{
-					
+		Stats stats = client.updateStats(s, new HashMap<String, Object>() {
+			{
+
 				put("Score", "-" + points);
-				
-			}});
+
+			}
+		});
 		System.out.println(stats);
 	}
 
 	public void getUserStadistic(String id) throws Exception {
-	Stats stats = client.getStats(id);
-	System.out.println(stats);
+		Stats stats = client.getStats(id);
+		System.out.println(stats);
 	}
-	
+
 	public Section getUserSection(String id) throws Exception {
 		Section section = client.getStatsSection(id, "Score");
-		//System.out.println(section + " section");
+		// System.out.println(section + " section");
 		return section;
 	}
-	
-	
+
 	public List<Rank> getLeaderBoard() throws Exception {
-		List<Rank> leaderB=  client.getLeaderboard("Score", 10); // Retorna los top 10 de la lista
+		List<Rank> leaderB = client.getLeaderboard("Score", 10); // Retorna los top 10 de la lista
 		System.out.println(leaderB + "test");
 		return leaderB;
 	}
